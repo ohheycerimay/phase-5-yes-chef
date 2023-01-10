b6f56623b7fa5b9951f7bceccb807741317ef66d
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-01-09 19:33:06 -0500

    rendered information to browser

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function List({ingredient}) {
     return (
-        <div className="ingredient-image">
-            {ingredient.name}
-            {ingredient.image}
+        <div>
+            <li>{ingredient.name}</li>
+           <img className="ingredient-image" src= {ingredient.image_url} alt={ingredient.name}/>
         </div>
     );
 }
