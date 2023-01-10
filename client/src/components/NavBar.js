9486859b75d3afe90938c70ae9a22e456ed8f347
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-01-10 18:24:13 -0500

    details

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom'
 
-function NavBar(props) {
-    return (
-        <div>
-            
+function NavBar({user}) {
+  
+  return (
+    <>
+      <div>
+        {user ? (
+            <div className = 'profile_container'>
+            <img className = 'profile_pic' src={user.profile_pic} alt={user.username} /> 
+            <h4 className='profile_greeting'>{user.username}</h4>
+            </div>
+        ): (
+            <div></div>)}
         </div>
-    );
+        <NavLink className="order-form-button" to="/orderform">Order Form</NavLink>
+        <NavLink className="logout-button" to="/logout">Logout</NavLink>
+    </>
+    )
 }
 
 export default NavBar;
--- client/src/components/OrderForm.js
+++ client/src/components/OrderForm.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function OrderForm(props) {
     return (
         <div>
-            
+           Hi I am an Order Form! 
+           <br></br>
+           <Link className="ingredients-link" to="/ingredients">Go Back</Link>
         </div>
     );
 }
