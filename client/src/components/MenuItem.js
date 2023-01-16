452c3d879b6b3891d3e083d209965533e3d96b37
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-01-16 18:43:06 -0500

    small changes

--- client/src/components/MenuItem.js
+++ client/src/components/MenuItem.js
@@ -3,18 +3,6 @@ import React from "react";
 function MenuItem({ item }) {
   console.log(item.ingredient);
 
-//   function updateStockAmount(ingredient) {
-//     fetch(`/ins/${ingredient.id}`, {
-//     method: "PATCH",
-//     headers,
-//     body: JSON.stringify({ likes: ++ingredient.amount }),
-//     }).then((r) => r.json())
-//     .then((freshPost) => {
-//         const newMenuArray = experie.posts.map((p) => p.id !== freshPost.id ? p : freshPost);
-//         setExperience({ ...experience, posts: newPostArray });
-//     });
-// }
-
   return (
     <div className="stock-card">
       {/* <img className="stock-image" src={item.ingredient.image_url} /> */}
