b5327003875294ec44ef8e8ba492dae8bf81f71a
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-01-20 14:12:03 -0500

    css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import YesChef from "./Yes Chef.png"
 
 function NavBar({ user }) {
   return (
     <>
       <div className="nav">
+        <img className="logo" src={YesChef} alt="YesChef"/>
       {/* <header>Yes Chef!</header> */}
         {user ? (
           
